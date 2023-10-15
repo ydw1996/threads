92d8dc00c887f42957d01da3779b14836c7b4071
ydw1996 <didekdnlt1996@gmail.com>
2023-10-16 01:17:24 +0900

    sahdecn/ui & zod library application

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      bio: "",
+    },
+  });
+  return <Form></Form>;
+};
+
+export default AccountProfile;
