8adc02b3e9cb20c0ad9a026b0a5df4fdd7933175
ydw1996 <didekdnlt1996@gmail.com>
2023-10-30 23:59:41 +0900

    uploadthing function update

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,7 +19,7 @@ import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod";
 import { ChangeEvent, useState } from "react";
 import { isBase64Image } from "@/lib/utils";
-
+import { useUploadThing } from "@/lib/uploadthing";
 interface Props {
   user: {
     id: string;
@@ -34,6 +34,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -69,13 +70,17 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
   };
 
-  function onSubmit(
+  const onSubmit = async (
     values: z.infer<typeof UserValidation>
-  ) {
+  ) => {
     const blob = values.profile_photo;
 
     const hasImageChanged = isBase64Image(blob);
-  }
+
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+    }
+  };
   return (
     <Form {...form}>
       <form
