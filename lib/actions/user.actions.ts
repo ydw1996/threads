b95655c3b3322f067e3c5596bfb7e3ef298fe4bf
ydw1996 <didekdnlt1996@gmail.com>
2023-11-02 23:56:36 +0900

    user.actions work

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,67 @@
 "use server";
 
-import { connectToDB } from "../validations/mongoose";
+// import { FilterQuery, SortOrder } from "mongoose";
+import { revalidatePath } from "next/cache";
 
-export async function updateUser(): Promise<void> {
-  {
+// import Community from "../models/community.model";
+// import Thread from "../models/thread.model";
+// import User from "../models/user.model";
+
+// import { connectToDB } from "../mongoose";
+
+export async function fetchUser(userId: string) {
+  try {
     connectToDB();
+
+    return await User.findOne({ id: userId }).populate({
+      path: "communities",
+      model: Community,
+    });
+  } catch (error: any) {
+    throw new Error(
+      `Failed to fetch user: ${error.message}`
+    );
+  }
+}
+
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
+
+export async function updateUser({
+  userId,
+  bio,
+  name,
+  path,
+  username,
+  image,
+}: Params): Promise<void> {
+  try {
+    connectToDB();
+
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(
+      `Failed to create/update user: ${error.message}`
+    );
   }
 }
